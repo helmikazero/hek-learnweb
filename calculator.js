c83711dadd2ae0bc755c59647507a7566bde7138
Helmika Priyanto <68269642+helmikazero@users.noreply.github.com>
2021-02-20 18:14:41 +0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,16 +1,21 @@
-function greeting(name, language) {
-    if(language === "English") {
-        return "Good Morning " + name + "!";
-    } else if (language === "French") {
-        return "Bonjour " + name + "!";
-    } else {
-        return "Selamat Pagi " + name + "!";
-    }
+const firstName = prompt("Siapa nama depanmu?");
+const lastName = prompt("Siapa nama belakangmu?");
+const language = prompt("Bisa berbahasa apa?");
+ 
+const user = {
+   name: {
+       first: firstName,
+       last: lastName,
+   },
+   language: language
+};
+ 
+if (user.language === "English") {
+   alert("Nice to meet you " + user.name.first + " " + user.name.last + "!");
+} else if (user.language === "French") {
+   alert("Ravi de vous rencontrer " + user.name.first + " " + user.name.last + "!");
+} else if (user.language === "Japanese") {
+   alert("Hajimemashite, " + user.name.first + " " + user.name.last + "!");
+} else {
+   alert("Senang bertemu dengan Anda " + user.name.first + " " + user.name.last + "!");
 }
-
-let greetingMessage = greeting("Harry", "French");
-console.log(greetingMessage);
-
-/* output
-Bonjour Harry!
-*/
