3b566a395d34902941924dcfed262dd019376cac
Helmika Priyanto <68269642+helmikazero@users.noreply.github.com>
2021-02-21 02:54:45 +0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,34 +1,46 @@
-console.log("This is a branch and i'm changing it now...");
-
 const calculator = {
-    displayNumber : '0',
-    operator : null,
-    firstNumber : null,
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
     waitingForSecondNumber: false
-};
-
-function updateDisplay() {
-    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
-}
-
-function clearCalculator() {
+ };
+  
+ function updateDisplay() {
+    document.querySelector("#displaynumber").innerText = calculator.displayNumber;
+ }
+  
+ function clearCalculator() {
     calculator.displayNumber = '0';
-    calculator.operator = null,
-    calculator.firstNumber = null,
-    calculator.waitingForSecondNumber = false
-}
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecondNumber = false;
+ }
+  
+ function inputDigit(digit) {
+    if(calculator.displayNumber === '0')
+    {
+        calculator.displayNumber = digit;
+    }
+    else
+    {
+        calculator.displayNumber += digit;
+    }
 
-function inputDigit(digit){
-    calculator.displayNumber += digit;
-}
-
-const buttons = document.querySelectorAll('.button'); //WATCH DIS
-for(let button of buttons){
-    button.addEventListener('click',function(event) {
+    // calculator.displayNumber += digit;
+ }
+  
+  
+ const buttons = document.querySelectorAll(".button");
+ for (let button of buttons) {
+    button.addEventListener('click', function(event) {
+  
+        // mendapatkan objek elemen yang diklik
         const target = event.target;
-
+  
+        console.log(target.innerText);
         inputDigit(target.innerText);
 
+        console.log(calculator.displayNumber);
         updateDisplay()
     });
-}
+ }
